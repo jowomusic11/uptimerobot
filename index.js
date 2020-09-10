fdfa6fe803396357f9b23d73ff8c603d5b7b84ca
jowomusic11 <71020965+jowomusic11@users.noreply.github.com>
2020-09-10 11:01:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require("axios")
-const urls = ["link project"]
+const urls = ["https://jowo-beat-music.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
         })
-    }, 60 * 1000);
+    }, 15 * 1000);
